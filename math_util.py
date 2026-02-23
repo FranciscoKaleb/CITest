465dbe1f08c682a69ef07c53b56b8ecedfb3bc34
FranciscoKaleb <franciscokaleb.marquez02@gmail.com>
2026-02-23 09:05:07 +0800

    removed bug

--- math_util.py
+++ math_util.py
@@ -3,7 +3,7 @@
 
 
 def add(a, b):
-    return a + b + 1
+    return a + b 
     
 def subtract(a, b):
     return a - b
@@ -15,4 +15,6 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+
 
